a7ca7316d84c21f8f1b163a59772b583989b7d18
Fetullah Furkan Özdemir <furkanozdemir51@yandex.com.tr>
2023-03-02 00:47:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+const firstPass = document.querySelector("#first-pass");
+const secondPass = document.querySelector("#second-pass");
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function getPass() {
+  let firstPassText = "";
+  let secondPassText = "";
+
+  for (let i = 0; i < 15; i++) {
+    const randomIndexOne = getRandomIndex(characters);
+    const randomIndexTwo = getRandomIndex(characters);
+    firstPassText += characters[randomIndexOne];
+    secondPassText += characters[randomIndexTwo];
+  }
+
+  firstPass.textContent = firstPassText;
+  secondPass.textContent = secondPassText;
+}
